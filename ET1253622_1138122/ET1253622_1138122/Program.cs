bf0d6bcdea708ce506fd64978a71c2681f0036a4
1253622-BryantTo <111195815+1253622-BryantTo@users.noreply.github.com>
2022-10-04 08:42:48 -0600

    SUBIR EJERCICIO TEORIA

---
+++ ET1253622_1138122/ET1253622_1138122/Program.cs
@@ -0,0 +1,33 @@
+﻿// Bryant Tó y Sebastian Echeverria
+int suma, multipicacion, potencia, N1, N2;
+
+Console.WriteLine("Ingrese dos numero");
+N1 = int.Parse(Console.ReadLine());
+N2 = int.Parse(Console.ReadLine());
+
+int Suma(int Valor1, int Valor2)
+{
+    return Valor1 + Valor2;
+}
+
+int multiplicacion(int Valor1, int valor2)
+{
+    var Contador = 0;
+    for (int i = 0; i < valor2; i++)
+    {
+       Contador = Suma(Contador, Valor1);
+    }
+    return Contador;
+}
+
+int Potencia(int Base, int Exponente)
+{
+    var Contador = 1;
+
+    for (int i = 0; i < Exponente; i++)
+    {
+        Contador = multiplicacion(Contador, Base);
+        
+    }
+    return Contador;
+}
---
+++ T8_1253622_BT/T8_1253622_BT/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T8_1253622_BT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            int A = 0;
+            int B = 1;
+            int C = 0;
+            int i = 2;
+            string resutado = "";
+
+            Console.WriteLine("Tarea #8");
+            if (N > 0)
+            {
+                
+            }
+
+        }
+    }
+}
