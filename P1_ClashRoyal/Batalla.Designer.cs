8a4a3733da793bcbb979098ef57e10e12bd81688
1253622-BryantTo <111195815+1253622-BryantTo@users.noreply.github.com>
2022-11-17 15:13:43 -0600

    Add files via upload

---
+++ P1_ClashRoyal/Batalla.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace P1_ClashRoyal
+{
+    partial class Batalla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Batalla));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtNombreJ1 = new System.Windows.Forms.Label();
+            this.txtMazoJ1 = new System.Windows.Forms.Label();
+            this.picMazoJ1 = new System.Windows.Forms.PictureBox();
+            this.picMazoJ2 = new System.Windows.Forms.PictureBox();
+            this.txtMazoJ2 = new System.Windows.Forms.Label();
+            this.txtNombreJ2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtContadorJ1 = new System.Windows.Forms.Label();
+            this.txtContadorJ2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMazoJ1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMazoJ2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1339, 750);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtNombreJ1
+            // 
+            this.txtNombreJ1.AutoSize = true;
+            this.txtNombreJ1.BackColor = System.Drawing.Color.Transparent;
+            this.txtNombreJ1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtNombreJ1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreJ1.Location = new System.Drawing.Point(16, 11);
+            this.txtNombreJ1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtNombreJ1.Name = "txtNombreJ1";
+            this.txtNombreJ1.Size = new System.Drawing.Size(100, 35);
+            this.txtNombreJ1.TabIndex = 1;
+            this.txtNombreJ1.Text = "Nombre:";
+            // 
+            // txtMazoJ1
+            // 
+            this.txtMazoJ1.AutoSize = true;
+            this.txtMazoJ1.BackColor = System.Drawing.Color.Transparent;
+            this.txtMazoJ1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtMazoJ1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMazoJ1.Location = new System.Drawing.Point(16, 65);
+            this.txtMazoJ1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtMazoJ1.Name = "txtMazoJ1";
+            this.txtMazoJ1.Size = new System.Drawing.Size(100, 35);
+            this.txtMazoJ1.TabIndex = 2;
+            this.txtMazoJ1.Text = "Nombre:";
+            // 
+            // picMazoJ1
+            // 
+            this.picMazoJ1.BackColor = System.Drawing.Color.Transparent;
+            this.picMazoJ1.Location = new System.Drawing.Point(16, 118);
+            this.picMazoJ1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picMazoJ1.Name = "picMazoJ1";
+            this.picMazoJ1.Size = new System.Drawing.Size(363, 204);
+            this.picMazoJ1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMazoJ1.TabIndex = 3;
+            this.picMazoJ1.TabStop = false;
+            // 
+            // picMazoJ2
+            // 
+            this.picMazoJ2.BackColor = System.Drawing.Color.Transparent;
+            this.picMazoJ2.Location = new System.Drawing.Point(959, 118);
+            this.picMazoJ2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picMazoJ2.Name = "picMazoJ2";
+            this.picMazoJ2.Size = new System.Drawing.Size(363, 204);
+            this.picMazoJ2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMazoJ2.TabIndex = 6;
+            this.picMazoJ2.TabStop = false;
+            // 
+            // txtMazoJ2
+            // 
+            this.txtMazoJ2.AutoSize = true;
+            this.txtMazoJ2.BackColor = System.Drawing.Color.Transparent;
+            this.txtMazoJ2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtMazoJ2.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMazoJ2.Location = new System.Drawing.Point(959, 65);
+            this.txtMazoJ2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtMazoJ2.Name = "txtMazoJ2";
+            this.txtMazoJ2.Size = new System.Drawing.Size(100, 35);
+            this.txtMazoJ2.TabIndex = 5;
+            this.txtMazoJ2.Text = "Nombre:";
+            // 
+            // txtNombreJ2
+            // 
+            this.txtNombreJ2.AutoSize = true;
+            this.txtNombreJ2.BackColor = System.Drawing.Color.Transparent;
+            this.txtNombreJ2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtNombreJ2.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreJ2.Location = new System.Drawing.Point(959, 11);
+            this.txtNombreJ2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtNombreJ2.Name = "txtNombreJ2";
+            this.txtNombreJ2.Size = new System.Drawing.Size(100, 35);
+            this.txtNombreJ2.TabIndex = 4;
+            this.txtNombreJ2.Text = "Nombre:";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(603, 197);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 71);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtContadorJ1
+            // 
+            this.txtContadorJ1.AutoSize = true;
+            this.txtContadorJ1.BackColor = System.Drawing.Color.Transparent;
+            this.txtContadorJ1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtContadorJ1.Font = new System.Drawing.Font("Segoe Script", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContadorJ1.Location = new System.Drawing.Point(309, 692);
+            this.txtContadorJ1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtContadorJ1.Name = "txtContadorJ1";
+            this.txtContadorJ1.Size = new System.Drawing.Size(50, 57);
+            this.txtContadorJ1.TabIndex = 8;
+            this.txtContadorJ1.Text = "0";
+            // 
+            // txtContadorJ2
+            // 
+            this.txtContadorJ2.AutoSize = true;
+            this.txtContadorJ2.BackColor = System.Drawing.Color.Transparent;
+            this.txtContadorJ2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtContadorJ2.Font = new System.Drawing.Font("Segoe Script", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContadorJ2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtContadorJ2.Location = new System.Drawing.Point(981, 692);
+            this.txtContadorJ2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtContadorJ2.Name = "txtContadorJ2";
+            this.txtContadorJ2.Size = new System.Drawing.Size(50, 57);
+            this.txtContadorJ2.TabIndex = 9;
+            this.txtContadorJ2.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(563, 370);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(235, 71);
+            this.button2.TabIndex = 10;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(1202, 706);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 44);
+            this.button3.TabIndex = 11;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(-1, 706);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 44);
+            this.button4.TabIndex = 12;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Batalla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 748);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtContadorJ2);
+            this.Controls.Add(this.txtContadorJ1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picMazoJ2);
+            this.Controls.Add(this.txtMazoJ2);
+            this.Controls.Add(this.txtNombreJ2);
+            this.Controls.Add(this.picMazoJ1);
+            this.Controls.Add(this.txtMazoJ1);
+            this.Controls.Add(this.txtNombreJ1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Batalla";
+            this.Text = "Batalla";
+            this.Load += new System.EventHandler(this.Batalla_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMazoJ1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMazoJ2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label txtNombreJ1;
+        private System.Windows.Forms.Label txtMazoJ1;
+        private System.Windows.Forms.PictureBox picMazoJ1;
+        private System.Windows.Forms.PictureBox picMazoJ2;
+        private System.Windows.Forms.Label txtMazoJ2;
+        private System.Windows.Forms.Label txtNombreJ2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Label txtContadorJ1;
+        public System.Windows.Forms.Label txtContadorJ2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
